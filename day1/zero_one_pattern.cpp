e17567b1b7abfdc6b6fc134c0eb3a6690c856610
Jayendra Naresh Ingale <jayendraingale01@outlook.com>
2021-01-01 22:17:04 +0530

    [day4] zero one pattern

---
+++ day1/zero_one_pattern.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+//  pattern 
+/**
+1
+0 1
+1 0 1
+0 1 0 1
+1 0 1 0 1
+*/
+  int rows = 5;
+  for (int i=1;i<=rows;i++){
+    for (int j=0;j<i;j++){
+      if ((i+j)%2 == 1){
+        cout<<'1';
+      }else{
+        cout<<'0';
+      }
+    };
+    cout<<endl;
+  };
+  return 0;
+}
